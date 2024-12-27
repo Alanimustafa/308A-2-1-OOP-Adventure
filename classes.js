4da1f7bdf62bbbc1e588b13fca215ce949025bbb
Alanimustafa <mustafa.kamel.ani@gmail.com>
2024-12-27 13:14:26 -0600

    Part 2 | Adding the roll  method to Character

---
+++ classes.js
@@ -0,0 +1,15 @@
+
+// Part 2 | Class Fantasy 
+
+// Part 2 | Character Class has been instantiated.
+class Character {
+  constructor (name) {
+    this.name = name;
+    this.health = 100;
+    this.inventory = [];
+  }
+  roll (mod = 0) {  // Part 2 | Adding the roll function as a method to Character
+    const result = Math.floor(Math.random() * 20) + 1 + mod;
+    console.log(`${this.name} rolled a ${result}.`)
+    }
+}
