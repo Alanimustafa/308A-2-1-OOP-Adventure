b47aa8c5679ac48e25ed3f3b36115ec66b8ee8ff
Alanimustafa <mustafa.kamel.ani@gmail.com>
2024-12-27 12:49:00 -0600

    Part 1 | Leo and his companion Frank have been added to Robin

--- app.js
+++ app.js
@@ -2,12 +2,24 @@ const adventurer = {
   name: "Robin",
   health: 10,
   inventory: ["sword", "potion", "artifact"],
+  companion: { // Part 1 | Robin's companion Leo has been added.
+    name: "Leo",
+    type: "Cat",
+    companion: { // Part 1 | Leo's companion Frank has been added.
+      name: "Frank",
+      type: "flea",
+      inventory: ["hat", "sunglasses"]
+    }
+  }
+
   }
 // Part 1 | Loging each item in Robin's inventory
 
 adventurer.inventory.forEach(element => {
   console.log(element);
 });
+
+console.log(adventurer);
 
 
 
