25eb5732d3a440f526dda6221c91e1f922e773e1
Alanimustafa <mustafa.kamel.ani@gmail.com>
2024-12-27 12:44:04 -0600

    Part 1 | Loging each item in Robin's inventory

---
+++ app.js
@@ -0,0 +1,13 @@
+const adventurer = {
+  name: "Robin",
+  health: 10,
+  inventory: ["sword", "potion", "artifact"],
+  }
+// Part 1 | Loging each item in Robin's inventory
+
+adventurer.inventory.forEach(element => {
+  console.log(element);
+});
+
+
+
